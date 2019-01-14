7c2b26647bc22bf3b1173ebc15110fe2288e9294
hanihani93 <hn_220@naver.com>
2019-01-14 09:59:55 +0900

    쇼핑몰

---
+++ public/js/main.js
@@ -0,0 +1,47 @@
+// Firebase Init / github에서 복사하신 분은 꼭 자신의 내용으로 바꿔주세요.
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDXXjJplUtkjqrLN41-Y3YHH-LaDCH8FFw",
+    authDomain: "shop01.firebaseapp.com",
+    databaseURL: "https://shop01.firebaseio.com",
+    projectId: "shop01",
+    storageBucket: "shop01.appspot.com",
+    messagingSenderId: "633572072347"
+  };
+  firebase.initializeApp(config);
+
+
+// top_nav hover 이벤트
+$(".top_icon").mouseenter(function(){
+	$(this).children("img").css({"opacity":.7});
+});
+$(".top_icon").mouseleave(function(){
+	$(this).children("img").css({"opacity":1});
+});
+
+// rt_wings 이벤트
+$(".top_nav .fa-bars").click(function(){
+	var $bg = $(".rt_bg");
+	var $cont = $(".rt_cont");
+	$bg.css({"opacity":0, "display":"block"}).stop().animate({"opacity":.3}, 1000);
+	$cont.css({"display":"block", "right":"-240px"}).stop().animate({"right":0}, 1000);
+});
+
+$(".rt_cont .fa-close").click(function(){
+	var $bg = $(".rt_bg");
+	var $cont = $(".rt_cont");
+	$bg.stop().animate({"opacity":0}, 800, function(){
+		$(this).css({"display":"none"});
+	});
+	$cont.stop().animate({"right":"-240px"}, 800, function(){
+		$(this).css({"display":"none"});
+	});
+});
+
+$(".rt_bg").click(function(e){
+	e.stopPropagation();
+	$(".rt_cont .fa-close").trigger("click");
+});
+
+//메인네비 / .navs
+//firebase.database().ref("root/test").push({test:"테스트"}).key;
